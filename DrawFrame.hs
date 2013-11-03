0c06b416a1d18d970d6945faabb7c5b505fb96b8
ToF- <cthibaut@octo.com>
2013-11-03 18:44:35 +0100

    includes a xml header in the SVG

--- DrawFrame.hs
+++ DrawFrame.hs
@@ -1,3 +1,5 @@
 module DrawFrame
 where
 
+drawSVGFrame :: Integer -> Integer -> [String]
+drawSVGFrame _ _ = ["<?xml version=\"1.0\" standalone=\"no\"?>"]
--- Specs.hs
+++ Specs.hs
@@ -2,9 +2,12 @@
 module Main
 where
 import Test.Hspec
+import DrawFrame
 
 main :: IO ()
 main = hspec $ do
+    let d = drawSVGFrame 100 200
     describe "Frame Drawer\n" $ do 
-        it "does nothing so far"  $ do
-            True `shouldBe` True
+        it "outputs a SVG file"  $ do
+            d !! 0 `shouldBe` "<?xml version=\"1.0\" standalone=\"no\"?>"
+            
