d62df2719e6d84b40107548816229a7e3c09a183
ToF- <cthibaut@octo.com>
2013-11-04 23:15:52 +0100

    draws a rectangle

--- DrawFrame.hs
+++ DrawFrame.hs
@@ -2,4 +2,7 @@ module DrawFrame
 where
 
 drawSVGFrame :: Integer -> Integer -> [String]
-drawSVGFrame _ _ = ["<?xml version=\"1.0\" standalone=\"no\"?>"]
+drawSVGFrame _ _ = ["<?xml version=\"1.0\" standalone=\"no\"?>",
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">",
+                    "<rect width=\"300\" height=\"100\" style=\"fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)\"/>",
+                    "</svg>" ]
--- Specs.hs
+++ Specs.hs
@@ -9,5 +9,9 @@ main = hspec $ do
     let d = drawSVGFrame 100 200
     describe "Frame Drawer\n" $ do 
         it "outputs a SVG file"  $ do
-            d !! 0 `shouldBe` "<?xml version=\"1.0\" standalone=\"no\"?>"
+            head d `shouldBe` "<?xml version=\"1.0\" standalone=\"no\"?>"
+            (head.tail) d `shouldBe` "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
+            (head.tail.tail) d `shouldBe` "<rect width=\"300\" height=\"100\" style=\"fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)\"/>"
+            last d `shouldBe` "</svg>"
+            
             
